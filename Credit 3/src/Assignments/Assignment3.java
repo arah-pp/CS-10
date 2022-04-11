8df39a8382dea755b6871dcc35ab2cfa1dc27160
16265010 <16265010@L815WD220026.edu.cbe.ab.ca>
2022-04-11 13:39:35 -0600

    Credit 3

--- Credit 3/src/Assignments/Assignment3.java
+++ Credit 3/src/Assignments/Assignment3.java
@@ -1,10 +1,28 @@
 package Assignments;
+/*
 
-public class Assignment3 {
+Program: ${Bingo Card}.java          Date: ${4/11/22}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+Author: Ashton Hirdle
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+public class Assignment3 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("  B    I    N    G    O\r\n"
+				+" \r\n"
+				+"  1   16   31   46   61\r\n"
+				+" 15   30   45   60   75\r\n"
+				+" 11   18  FREE  50   69\r\n"
+				+"  7   22   42   57   67\r\n"
+				+"  3   26   37   55   73");
+				
+		}
 
 }
