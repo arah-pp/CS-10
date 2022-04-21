22ec28a95185a925accef762967ef2401ff9d450
16265010 <16265010@L815WD220026.edu.cbe.ab.ca>
2022-04-21 14:58:06 -0600

    Credit4

--- Credit 3/src/Assignments/Assignment5.java
+++ Credit 3/src/Assignments/Assignment5.java
@@ -1,5 +1,15 @@
 package Assignments;
+/*
 
+Program: ${Assignment5}.java          Date: ${4/21/2022}
+
+
+Author: Ashton Hirdle 
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
 import java.util.Scanner;
 
 public class Assignment5 
@@ -7,12 +17,18 @@ public class Assignment5
 
 	public static void main(String[] args) 
 	{
-         Scanner input = new Scanner(System.in); //This will scan the users input
-	
-         System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = 145\r\n"
-        		 +"(179 + 21 + 10) / 7 + 181 = 211\r\n"
-        		 +"10389 * 56 * 11 + 2246 = 6401870");
+         int X = 79 + 3 * (4 + 82 - 68) - 7 + 19; //Documenting and solving the equation and saving as "X"
+         int Y = (179 +21 +10) / 7 + 181; // Solving the equation and saving as "Y"
+         int Z = 10389 * 56 * 11 + 2246; //Solving Equation and saving as "Z"
+	     System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + X); //Displaying the final equation and answer
+         System.out.println("(179 +21 +10) / 7 + 181 = " + Y); //Displaying the final equation and answer
+         System.out.println("10389 * 56 * 11 + 2246 = " + Z); //Displaying the final equation and answer
          
 	}
 
 }
+/* 
+79 + 3 * (4 + 82 - 68) - 7 + 19 = 145
+(179 +21 +10) / 7 + 181 = 211
+10389 * 56 * 11 + 2246 = 6401870
+ */
