3b3b008be17631d7e8c2feb1fc53fb324dd58593
16265010 <16265010@L815WD220026.edu.cbe.ab.ca>
2022-04-27 13:42:04 -0600

    Credit4

--- Credit 3/src/Assignments/Assignment7.java
+++ Credit 3/src/Assignments/Assignment7.java
@@ -18,23 +18,23 @@ public class Assignment7
 	public static void main(String[] args) 
 	{
 
-		Scanner input = new Scanner (System.in);
+		Scanner input = new Scanner (System.in); //Scanning the users input
 		
-		System.out.println("Enter the change in cents: ");
+		System.out.println("Enter the change in cents: "); //Displaying this text
 		
-		int change = input.nextInt();
-		int quarters = change / 25;
-		int dimes = (change % 25)/10;
-		int nickels = ((change % 25)%10)/5;
-		int remainder = (((change % 25)%10)%5)/1;
+		int change = input.nextInt(); //Taking in the total amount of change
+		int quarters = change / 25; // Calculating the amount of quarters
+		int dimes = (change % 25)/10; //Taking the remaining number and dividing by the amount of dimes
+		int nickels = ((change % 25)%10)/5; //Taking the remaining change and calculating the final amount of nickels
+		int remainder = (((change % 25)%10)%5)/1; //Calculating and giving back the extra amount of pennies
 		
-		System.out.println("The amount of change you provided is: " + change);
-		System.out.println("\r\n");
-		System.out.println("The number of coins back:");
-		System.out.println("Quarters: " + quarters);
-		System.out.println("Dimes: " + dimes);
-		System.out.println("Nickels: " + nickels);
-		System.out.println("Pennies: "+ remainder);
+		System.out.println("The amount of change you provided is: " + change); //Displaying the amount of change the user inputed
+		System.out.println("\r\n"); //Spacing a line
+		System.out.println("The number of coins back:"); //Displaying "The number of coins back:"
+		System.out.println("Quarters: " + quarters); //Showing the amount of quarters deciphered from the calculations
+		System.out.println("Dimes: " + dimes); //Showing the total amount of dimes they user gets
+		System.out.println("Nickels: " + nickels); //Showing the amount of nickels the user gets back
+		System.out.println("Pennies: "+ remainder); //Showing the amount of pennies the user has left over
 		
 	}
 
