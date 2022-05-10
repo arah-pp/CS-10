675a090d8e858ba7b197d9b2ba370caa3e4f621e
16265010 <16265010@L815WD220026.edu.cbe.ab.ca>
2022-05-10 14:36:10 -0600

    Credit5

--- Credit 3/src/Credit5/Assignment14.java
+++ Credit 3/src/Credit5/Assignment14.java
@@ -14,30 +14,30 @@ public class Assignment14
 
 	public static void main(String[] args) 
 	{
-		Scanner userInput = new Scanner(System.in);
+		Scanner userInput = new Scanner(System.in); //This is the scanner
 		
-		System.out.println("Please enter you age: ");
-		int age = userInput.nextInt();
+		System.out.println("Please enter you age: "); //Asking user to enter their age
+		int age = userInput.nextInt(); //Scanning the users input
 		
-		if (age > 18)
+		if (age > 18) //If the age is greater than 18
 		{
-			System.out.println("You are a Adult");
+			System.out.println("You are a Adult"); //Text displays for previous statement
 		}
-		else if (age > 12)
+		else if (age > 12) //If the age is greater than 12
 		{
-			System.out.println("You are a Teen");
+			System.out.println("You are a Teen"); //Text displays for previous statement
 		}
-		else if (age >= 11)
+		else if (age >= 11) //If the age is greater than or equal to 11
 		{
-			System.out.println("You are a Preteen");
+			System.out.println("You are a Preteen"); //Text displays for previous statement
 		}
-		else if (age >= 6)
+		else if (age >= 6) //If the age is  greater than or equal to 6
 		{
-			System.out.println("You are a Child");
+			System.out.println("You are a Child"); //Text displays for previous statement
 		}
-		else if (age >=1)
+		else if (age >=1) //If the age is greater then or equal to 1
 		{
-			System.out.println("You are a Toddler");
+			System.out.println("You are a Toddler"); //Text displays for previous statement
 		}
 		
 
