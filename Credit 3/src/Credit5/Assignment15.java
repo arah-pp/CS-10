120d262bb38d950dc0106a13e04a7d1499fb3a88
16265010 <16265010@L815WD220026.edu.cbe.ab.ca>
2022-05-16 12:37:04 -0600

    Credit5

--- Credit 3/src/Credit5/Assignment15.java
+++ Credit 3/src/Credit5/Assignment15.java
@@ -15,13 +15,13 @@ public class Assignment15
 	public static void main(String[] args) 
 	{
 		
-		int frn = (int) (Math.random()*5); //This is the generator for the  First Random Numbers
-		int srn = 1 + (int) (Math.random()*10); //This is the Second Random Number generator
+		int X = (int) (Math.random()*5); //This is the generator for the  First Random Number
+		int Y = 1 + (int) (Math.random()*10); //This is the Second Random Number generator
 		
 		Scanner userinput = new Scanner(System.in); //This is the scanner
-		System.out.println("What is  " + frn + " * " + srn); //Asking the user what is "frn" * "srn"
+		System.out.println("What is  " + X + " * " + Y); //Asking the user what is "X" * "Y"
 		int input = userinput.nextInt(); //Scanning the users input
-				if ( input == frn * srn) // If the users input is correct 
+				if ( input == X * Y) // If the users input is correct 
 				{
 					System.out.println("You got it right! Congratulations!!" ); //Displays on screen if correct
 				}
@@ -31,7 +31,7 @@ public class Assignment15
 					System.out.println("You got it wrong. Please Try again." ); //Displays if wrong
 					int input1 = userinput.nextInt(); //Lets you try again so need another scanner
 					
-					if (input1 == frn * srn) //If you get it correct
+					if (input1 == X * Y) //If you get it correct
 					{
 						System.out.println("You got it right! Congratulations!!" ); //Displays if your correct
 
@@ -51,7 +51,9 @@ What is  4 * 7
 28
 You got it right! Congratulations!!
 
-What is  3 * 4
-4
+What is  4 * 8
+36
 You got it wrong. Please Try again.
+32
+You got it right! Congratulations!!
  */
